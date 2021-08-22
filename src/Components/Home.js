e00a8f9d5bc8550c82672d78b2b535af22ce6bdd
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-08-22 22:33:34 +0100

    Adding nav bar component

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Home(props) {
+  return (
+    <div>
+      <Nav />
+      {/* <div>This is home!</div> */}
+    </div>
+  );
+}
+
+export default Home;
