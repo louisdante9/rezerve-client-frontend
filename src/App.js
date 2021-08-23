c5b6bb109e225218373baecaa60d1c51e7a38e51
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-08-23 21:30:14 +0100

    Skeleton for auth pages(login/signup)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
-
   );
 }
 
