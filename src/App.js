4bcc81d6acd886b6c775a40477bba70b3af06363
louis nwamadi <louisdante9@gmail.com>
2021-08-25 22:04:35 +0100

    add new home page template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/home/Home";
-import Register from "./Components/Register";
-import Login from "./Components/Login";
-import ForgotPassword from "./Components/ForgotPassword";
+import Register from "./Components/auth/Registration/Register";
+import Login from "./Components/auth/Login/Login";
+import ForgotPassword from "./Components/auth/ForgotPassword";
+import About from "./Components/others/About";
+import Contact from "./Components/others/Contact";
+import Faq from "./Components/others/Faq";
 import Listings from "./Components/Listings";
 import ListSingle from "./Components/ListSingle";
+import Bookings from "./Components/user/Bookings";
+import Favourite from "./Components/user/Favourite";
+import Reviews from "./Components/user/Reviews";
 import NotFound from "./Components/NotFound";
 import Layout from "./Components/Layout";
+import Checkout from "./Components/others/Checkout";
+import Comingsoon from "./Components/others/Comingsoon";
+import VerifyUser from "./Components/auth/VeirifyUser";
 
 function App() {
   return (
@@ -15,6 +24,9 @@ function App() {
       <Switch>
         <Route path="/forgot-password">
           <ForgotPassword />
+        </Route>
+        <Route path="/verification">
+          <VerifyUser />
         </Route>
         <Route path="/login">
           <Login />
@@ -28,6 +40,30 @@ function App() {
           </Route>
           <Route path="/listings">
             <Listings />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Route path="/faq" exact>
+            <Faq />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout />
+          </Route>
+          <Route path="/coming-soon" exact>
+            <Comingsoon />
+          </Route>
+          <Route path="/bookings" exact>
+            <Bookings />
+          </Route>
+          <Route path="/favourite" exact>
+          < Favourite />
+          </Route>
+          <Route path="/reviews" exact>
+            <Reviews />
           </Route>
           <Route path="/" exact>
             <Home />
