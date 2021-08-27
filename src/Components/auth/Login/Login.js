e652e4b2c964271e4b6362da5989fbacee99b07e
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-08-27 22:36:18 +0100

    Remove console statement

--- src/Components/auth/Login/Login.js
+++ src/Components/auth/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
@@ -9,7 +9,6 @@ function Login(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { errors: error } = useSelector((state) => state.setCurrentUser);
-  console.log(error, "%%");
   const [loading, setLoading] = useState(false);
   const { handleBlur, handleChange, handleSubmit, errors } = useFormik({
     initialValues: {
